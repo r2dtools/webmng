235b0466fd3ed760487270c7feb2ceadc2635194
Alexander Tikhonov <alex.vl.tihon@gmail.com>
2022-10-26 23:28:40 +0700

    Added test for options

--- pkg/options/options_test.go
+++ pkg/options/options_test.go
@@ -1,1 +1,26 @@
 package options
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGet(t *testing.T) {
+	params := map[string]string{
+		"param1": "value1",
+	}
+	defaults := map[string]string{
+		"param1": "value1_1",
+		"param2": "value2",
+	}
+
+	options := Options{
+		Params:   params,
+		Defaults: defaults,
+	}
+
+	assert.Equal(t, "value1", options.Get("param1"))
+	assert.Equal(t, "value2", options.Get("param2"))
+	assert.Equal(t, "", options.Get("param3"))
+}
